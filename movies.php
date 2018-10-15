f0a90e88582c940af34632c2a6c1d542328a07df
Matheus Ribeiro <30762136+matheusribeirog@users.noreply.github.com>
2018-10-15 08:31:13 -0300

    Update movies.php

--- movies.php
+++ movies.php
@@ -1,40 +1,52 @@
-<?php
-global $contador; /* Essa Variável será utilizada  para contar quantos filmes serão mostrados na tela*/
-$curl = curl_init();
-/*Realizando a conexão com a API*/
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "http://api.themoviedb.org/3/movie/popular?api_key=YourKeyHERE",//add your the movie db key here
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_POSTFIELDS => "{}",
-));
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>The Movie DB - API</title>
+</head>
+<body>
+	<?php
+	global $contador; /* Essa Variável será utilizada  para contar quantos filmes serão mostrados na tela*/
+	$curl = curl_init();
+	/*Realizando a conexão com a API*/
+	curl_setopt_array($curl, array(
+	  CURLOPT_URL => "http://api.themoviedb.org/3/movie/popular?api_key=YourKeyHERE",//add your the movie db key here
+	  CURLOPT_RETURNTRANSFER => true,
+	  CURLOPT_ENCODING => "",
+	  CURLOPT_MAXREDIRS => 10,
+	  CURLOPT_TIMEOUT => 30,
+	  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	  CURLOPT_CUSTOMREQUEST => "GET",
+	  CURLOPT_POSTFIELDS => "{}",
+	));
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
 
-curl_close($curl);
+	curl_close($curl);
 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else { /*Caso a conexão seja feita com sucesso ele roda o código abaico */
-    /* Decodifica o JSON */
-	$obj = json_decode($response);
-	
-	foreach($obj->{'results'} as $valor){	
-		print_r ("<br> <img width='150px' src='https://image.tmdb.org/t/p/w500/".$valor->{'poster_path'}."'>".'<br>'); /* Concatena a imagem para ser mostrada na tela */
-		print_r ('<br> <h1>'.$valor->{'original_title'}.'</h1>');
-		print_r ('<p>'.$valor->{'overview'}.'</p>');	
-		if ($contador >= 20){ /*Esse contador estipula quantos filmes serão mostrados na tela*/
-			break;
+	if ($err) {
+	  echo "cURL Error #:" . $err;
+	} else { /*Caso a conexão seja feita com sucesso ele roda o código abaico */
+	    /* Decodifica o JSON */
+		$obj = json_decode($response);
+
+		foreach($obj->{'results'} as $valor){	
+			print_r ("<br> <img width='150px' src='https://image.tmdb.org/t/p/w500/".$valor->{'poster_path'}."'>".'<br>'); /* Concatena a imagem para ser mostrada na tela */
+			print_r ('<br> <h1>'.$valor->{'original_title'}.'</h1>');
+			print_r ('<p>'.$valor->{'overview'}.'</p>');	
+			if ($contador >= 20){ /*Esse contador estipula quantos filmes serão mostrados na tela*/
+				break;
+			}
+			$contador ++;
 		}
-		$contador ++;
+
 	}
 	
-}
+</body>
+</html>
 
 
 
